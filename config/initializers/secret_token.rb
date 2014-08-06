edd016f97f903c8a803b53f7731a74f2fe0f6e3b
Gabe G'Sell <gabergg@gmail.com>
2014-08-06 15:18:49 -0700

    clean sweep of repo to run with 2.0.0 and 4.0.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimplePortfolio::Application.config.secret_key_base = '9ebceef81b25307f1a169911db100994a0dc45a0e1a4b57976e09a726ca996631dc4069993b43268d434caaa5483217fffd7e9be961ce0169284644f4f37c9e8'
